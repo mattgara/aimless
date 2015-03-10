40349da3b8719e66c6e51c3181b17bd92239bff7
aaron zimmer <aaron.a.zimmer@gmail.com>
2015-03-09 22:42:17 -0700

    working version with rate 0.5 95 percent accuracy

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -6,7 +6,7 @@ def getTrainingLabels():
 	number_of_images	= f.read(4)
 	noi					= unpack(">i",number_of_images)
 	mn					= unpack(">i",magic_number)
-	rest = unpack(">"+("B"*mn[0]),f.read(mn[0]))
+	rest = unpack(">"+("B"*noi[0]),f.read(noi[0]))
 	f.close()
 	return noi[0],np.array(rest)	
 
@@ -33,7 +33,7 @@ def getTestLabels():
 	number_of_images	= f.read(4)
 	noi					= unpack(">i",number_of_images)
 	mn					= unpack(">i",magic_number)
-	rest = unpack(">"+("B"*mn[0]),f.read(mn[0]))
+	rest = unpack(">"+("B"*noi[0]),f.read(noi[0]))
 	f.close()
 	return noi[0],np.array(rest)	
 
@@ -75,49 +75,89 @@ def classify(image,weights_1,bias_1,weights_2,bias_2):
 
 	activations_1 = np.zeros(num_hiddens)
 	for i_hidden in range(num_hiddens):
-		activations_1[i_hidden]=computeActivation(image.flatten(),weights_1[:,i_hidden].flatten(),bias_1)
+		activations_1[i_hidden]=computeActivation(image.flatten(),weights_1[:,i_hidden].flatten(),bias_1[i_hidden])
 
 	output = np.zeros(num_outputs)
 	for i_output in range(num_outputs):
-		output[i_output]=computeActivation(activations_1.flatten(),weights_2[:,i_output].flatten(),bias_2)
+		output[i_output]=computeActivation(activations_1.flatten(),weights_2[:,i_output].flatten(),bias_2[i_output])
 
 	return output,activations_1
 
 ##### train brain #######
 
 def trainBrain(images,labels,diameter=28,hidden_diameter=24,iterations=-1,rate=0.01):
-	weights_1 	= np.random.rand(28*2,24*2)
-	bias_1 		= np.zeros(24*2)
-	weights_2 	= np.random.rand(24*2,10)
+	weights_1 	= np.random.rand(diameter**2,hidden_diameter**2)/1000.
+	bias_1 		= np.zeros(hidden_diameter**2)
+	weights_2 	= np.random.rand(hidden_diameter**2,10)/1000.
 	bias_2 		= np.zeros(10)
-
-	delta_weights_1 	= np.zeros((28*2,24*2))
-	delta_bias_1 		= np.zeros(24*2)
-	delta_weights_2 	= np.zeros((24*2,10))
-	delta_bias_2 		= np.zeros(10)
 
 	if iterations == -1:
 		iterations = len(images)
 
+	label_list = []
+	for i in range(10):
+		label_list.append(np.zeros(10))
+		label_list[i][i]=1
+
+	sum_total = 0
+	sum_correct = 0
 	for i in range(iterations):
-		output,activations_1 = classify(images[i],weights_1,bias_1,weights_2,bias_2)
-		weights_2 -= rate * np.abs(output-)
+		output,activations_1 = classify(images[i]/256.,weights_1,bias_1,weights_2,bias_2)
+
+		predicted = np.where(output==output.max())[0][0]
+		sum_total+=1
+		if predicted==labels[i]:
+			sum_correct+=1
+		print labels[i],predicted,output,sum_correct/float(sum_total)
+
+		delta_weights_1 	= np.zeros((diameter**2,hidden_diameter**2))
+		delta_bias_1 		= np.zeros(hidden_diameter**2)
+		delta_weights_2 	= np.zeros((hidden_diameter**2,10))
+		delta_bias_2 		= np.zeros(10)
+		for j in range(10):
+			cost_derivative			= output[j]-label_list[labels[i]][j]
+			
+			delta_bias_2[j]			+= cost_derivative * logistic_prime(output[j])
+			delta_weights_2[:,j]	+= cost_derivative * logistic_prime(output[j]) * activations_1
+
+			for k in range(hidden_diameter**2):	
+				delta_bias_1[k]					+= cost_derivative * logistic_prime(output[j]) * weights_2[k,j] * logistic_prime(activations_1[k])
+				delta_weights_1[:,k]			+= cost_derivative * logistic_prime(output[j]) * weights_2[k,j] * logistic_prime(activations_1[k]) * images[i].flatten()
+
+		weights_1 	= weights_1 	- rate * delta_weights_1
+		bias_1 		= bias_1 		- rate * delta_bias_1
+		weights_2 	= weights_2 	- rate * delta_weights_2
+		bias_2 		= bias_2		- rate * delta_bias_2
+
+	return weights_1,bias_1,weights_2,bias_2
 
 
+def applyModel(images,labels,weights_1,bias_1,weights_2,bias_2):
+	iterations = len(images)
+
+	label_list = []
+	for i in range(10):
+		label_list.append(np.zeros(10))
+		label_list[i][i]=1
+
+	sum_total = 0
+	sum_correct = 0
+	for i in range(iterations):
+		output,activations_1 = classify(images[i]/256.,weights_1,bias_1,weights_2,bias_2)
+
+		predicted = np.where(output==output.max())[0][0]
+		sum_total+=1
+		if predicted==labels[i]:
+			sum_correct+=1
+		print labels[i],predicted,output,sum_correct/float(sum_total)
 
 
+###################### visualization routines ###########################################
 
+def viewWeights(weights,inw,inh,outw,outh):
+	pic = np.zeros((inh*outh,inw*outw))
+	for j in range(outh):
+		for i in range(outw):
+			pic[j*inh:(j+1)*inh,i*inw:(i+1)*inw]=weights[:,j*outw+i].reshape(inh,inw)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	plt.imshow(pic,interpolation="nearest"); plt.colorbar(); plt.show()
